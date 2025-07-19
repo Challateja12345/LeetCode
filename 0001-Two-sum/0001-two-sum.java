072915358fdbd02f7ca1ca461eb8552f9e96dde7
Challa Divya Teja Naga Venkata Prasad <22p31a1225@acet.ac.in>
2025-07-19 18:47:47 +0530

    Update 0001-two-sum.java

--- 0001-Two-sum/0001-two-sum.java
+++ 0001-Two-sum/0001-two-sum.java
@@ -1,1 +1,19 @@
-mm
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i]; 
+            int complement = target - current; 
+            if (indexMap.containsKey(complement)) {
+                return new int[] {indexMap.get(complement), i};
+            }
+            indexMap.put(current, i);
+        }
+       
+        throw new IllegalArgumentException("No two sum solution found");
+    }
+}
